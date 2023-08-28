5bdffd7e5f13adcc2328c3308dae4a8e4ed4802e
unknown <ayeshashaikh0222@gmail.com>
2023-08-28 20:09:53 +0530

    files added

---
+++ index.js
@@ -0,0 +1,26 @@
+// variables
+
+let openBtn = document.getElementById('open-btn');
+let modelContainer = document.getElementById('model-container');
+let closeBtn = document.getElementById('close-btn');
+
+// EventListener
+
+openBtn.addEventListener('click', function(){
+
+    modelContainer.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', function(){
+    
+    modelContainer.style.display = 'none';
+
+});
+
+
+window.addEventListener('click', function(e){
+
+    if(e.target === modelContainer){
+        modelContainer.style.display = 'none';
+    }
+});
